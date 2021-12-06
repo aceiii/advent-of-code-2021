fdfc8922b2492ec1c2a5f679f4764708977e6856
Borin Ouch <borin.ouch@gmail.com>
2021-12-05 23:46:08 -0800

    Add day2 part 1

---
+++ python/day2.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def parse_command(line):
+    cmd, amount = line.split(' ', 1)
+    return (cmd, int(amount, 10))
+
+
+def parse_commands(lines):
+    return list(map(parse_command, lines))
+
+
+def run_commands(commands, start):
+    x, y = start
+    while commands:
+        cmd, amount = commands.pop(0)
+        if cmd == 'forward':
+            x += amount
+        elif cmd == 'backward':
+            x -= amount
+        elif cmd == 'up':
+            y -= amount
+        elif cmd == 'down':
+            y += amount
+
+    return x, y
+
+
+def part1(lines):
+    commands = parse_commands(lines)
+    x, y = run_commands(commands, (0, 0))
+    return x * y
+
+
+def part2(lines):
+    pass
+
+
+def main():
+    lines = sys.stdin.readlines()
+    print("Part1: {}".format(part1(lines)))
+    print("Part2: {}".format(part2(lines)))
+
+
+if __name__ == "__main__":
+    main()
+
