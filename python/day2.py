62a4bc0af6098570a6bbba11dc9ab6f7c988e541
Borin Ouch <borin.ouch@gmail.com>
2021-12-05 23:51:33 -0800

    Add day2 part 2

--- python/day2.py
+++ python/day2.py
@@ -18,12 +18,26 @@ def run_commands(commands, start):
         cmd, amount = commands.pop(0)
         if cmd == 'forward':
             x += amount
-        elif cmd == 'backward':
-            x -= amount
         elif cmd == 'up':
             y -= amount
         elif cmd == 'down':
             y += amount
+
+    return x, y
+
+
+def run_commands2(commands, start):
+    x, y = start
+    aim = 0
+    while commands:
+        cmd, amount = commands.pop(0)
+        if cmd == 'forward':
+            x += amount
+            y += aim * amount
+        elif cmd == 'up':
+            aim -= amount
+        elif cmd == 'down':
+            aim += amount
 
     return x, y
 
@@ -35,7 +49,9 @@ def part1(lines):
 
 
 def part2(lines):
-    pass
+    commands = parse_commands(lines)
+    x, y = run_commands2(commands, (0, 0))
+    return x * y
 
 
 def main():
