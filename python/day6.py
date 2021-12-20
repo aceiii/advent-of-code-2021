83b0b6b30384f0583860a15a0e3512af980533fe
Borin Ouch <borin.ouch@gmail.com>
2021-12-20 14:42:23 -0800

    Add day6 part 2

--- python/day6.py
+++ python/day6.py
@@ -22,8 +22,43 @@ class LanternFish:
         self._timer -= 1
 
 
+class LanternFishWithCount(LanternFish):
+    def __init__(self, n, qty=1):
+        super().__init__(n)
+        self._qty = qty
+
+    def add(self, qty):
+        self._qty += qty
+
+    def quantity(self):
+        return self._qty
+
+    def tick(self):
+        new_fish = super().tick()
+        if new_fish:
+            return LanternFishWithCount(new_fish._timer, self._qty)
+
+    def __str__(self):
+        return f"({self._timer}: {self._qty})"
+
+    def __eq__(self, other):
+        return other._timer == self._timer
+
+
 def parse_fish(line):
-    return [LanternFish(int(n, 10)) for n in line.strip().split(',')]
+    return [LanternFishWithCount(int(n, 10)) for n in line.strip().split(',')]
+
+
+def parse_fish2(line):
+    nums = [int(n, 10) for n in line.strip().split(',')]
+    fish_map = {idx: None for idx in range(days_between_birth)}
+    for num in nums:
+        fish = fish_map[num]
+        if not fish:
+            fish_map[num] = LanternFishWithCount(num, 1)
+        else:
+            fish.add(1)
+    return [fish for fish in fish_map.values() if fish]
 
 
 def part1(lines):
@@ -40,8 +75,26 @@ def part1(lines):
 
     return len(fish)
 
+
 def part2(lines):
-    pass
+    fish = parse_fish2(lines[0])
+    days = 256
+    while days > 0:
+        days -= 1
+        new_fish = []
+        for f in fish:
+            nf = f.tick()
+            if nf:
+                new_fish.append(nf)
+        for nf in new_fish:
+            for f in fish:
+                if f == nf:
+                    f.add(nf.quantity())
+                    break
+            else:
+                fish.append(nf)
+
+    return sum(f.quantity() for f in fish)
 
 
 def main():
