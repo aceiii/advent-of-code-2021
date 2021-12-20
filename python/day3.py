96de50a09fb5c7b752c0ceb86fb1b6840ebe0e48
Borin Ouch <borin.ouch@gmail.com>
2021-12-20 06:48:39 -0800

    Add day3 part 2

--- python/day3.py
+++ python/day3.py
@@ -28,7 +28,45 @@ def part1(lines):
 
 
 def part2(lines):
-    pass
+    candidates = list(map(lambda x: x.strip(), lines))
+    oxygen_generator_rating = find_rating(candidates, oxygen_bit_criteria)
+    co2_scrubber_rating = find_rating(candidates, co2_bit_criteria)
+    life_support_rating = oxygen_generator_rating * co2_scrubber_rating
+    return life_support_rating
+
+
+def find_rating(candidates, candidate_func):
+    bit_index = 0
+    while len(candidates) > 1:
+        bits = map(lambda c: c[bit_index], candidates)
+        ones, zeroes = count_bits(bits)
+        bit = candidate_func(ones, zeroes)
+        candidates = list(filter(lambda c: int(c[bit_index], 10) == bit, candidates))
+        bit_index += 1
+    rating = int(candidates[0], 2)
+    return rating
+
+
+def count_bits(bits):
+    ones, zeroes = 0, 0
+    for b in bits:
+        if b == '1':
+            ones += 1
+        else:
+            zeroes += 1
+    return (ones, zeroes)
+
+
+def oxygen_bit_criteria(ones, zeroes):
+    if ones >= zeroes:
+        return 1
+    return 0
+
+
+def co2_bit_criteria(ones, zeroes):
+    if zeroes <= ones:
+        return 0
+    return 1
 
 
 def main():
