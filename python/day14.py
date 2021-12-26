4683d5985e09f11feb91b7c064fca806f8f3e08b
Borin Ouch <borin.ouch@gmail.com>
2021-12-25 16:50:28 -0800

    Add day14 part 2

--- python/day14.py
+++ python/day14.py
@@ -2,6 +2,7 @@
 
 import sys
 from operator import itemgetter
+from collections import defaultdict
 
 
 def parse_polymerization(lines):
@@ -30,12 +31,48 @@ def polymer_insertion(polymer, insertion_map):
     return "".join(new_polymer)
 
 
+
+class FastPolymerizer:
+    def __init__(self, template, insertion_map):
+        self._insertion_map = {tuple(key): value for key, value in insertion_map.items()}
+        self._pairs = defaultdict(lambda: 0)
+        self._elements = defaultdict(lambda: 0)
+        polymer = list(template)
+        prev = polymer.pop(0)
+        self._elements[prev] += 1
+        while polymer:
+            current = polymer.pop(0)
+            pair = (prev, current)
+            self._pairs[pair] += 1
+            self._elements[current] += 1
+            prev = current
+
+    def step(self):
+        new_pairs = defaultdict(lambda: 0)
+        for pair in self._pairs:
+            count = self._pairs[pair]
+            if pair in self._insertion_map:
+                left, right = pair
+                inserted = self._insertion_map[pair]
+                new_pair1 = (left, inserted)
+                new_pair2 = (inserted, right)
+                new_pairs[new_pair1] += count
+                new_pairs[new_pair2] += count
+                self._elements[inserted] += count
+            else:
+                new_pairs[pair] += count
+        self._pairs = new_pairs
+
+    def elements(self):
+        return dict(self._elements)
+
+
 def part1(lines):
     polymer, insertion_map = parse_polymerization(lines)
     steps = 10
     for _ in range(steps):
         polymer = polymer_insertion(polymer, insertion_map)
-    counts = {c:polymer.count(c) for c in polymer}
+    counts = {c: polymer.count(c) for c in polymer}
     sorted_counts = sorted(((c, counts[c]) for c in counts), key=itemgetter(1))
     _, max_elements = sorted_counts[-1]
     _, min_elements = sorted_counts[0]
@@ -43,7 +80,16 @@ def part1(lines):
 
 
 def part2(lines):
-    pass
+    template, insertion_map = parse_polymerization(lines)
+    polymerizer = FastPolymerizer(template, insertion_map)
+    steps = 40
+    for _ in range(steps):
+        polymerizer.step()
+    elements = polymerizer.elements()
+    sorted_counts = sorted(elements.items(), key=itemgetter(1))
+    _, max_elements = sorted_counts[-1]
+    _, min_elements = sorted_counts[0]
+    return max_elements - min_elements
 
 
 def main():
